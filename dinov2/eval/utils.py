27713c860a2603e0d85064762a0d68fbb8021d95
mprzewie <m.przewie@gmail.com>
2025-04-02 18:35:47 +0200

    fix

--- dinov2/eval/utils.py
+++ dinov2/eval/utils.py
@@ -66,7 +66,7 @@ def evaluate(
     for samples, targets, *_ in metric_logger.log_every(data_loader, 10, header):
         if isinstance(targets, list):
             targets, prompts = targets
-            outputs = model(samples.to(device), register_prompts=prompts)
+            outputs = model(samples.to(device), register_prompts=prompts.to(device))
 
         targets = targets.to(device)
 
